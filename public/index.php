194432822813f861da0263c9332dda66c04c05c7
Francisco Felix <ffelix@desveladisimo.com>
2019-06-07 08:33:55 -0700

    Enable public access for the home page

--- public/index.php
+++ public/index.php
@@ -11,10 +11,6 @@ $config = ConfigFactory::getInstance("dotenv");
 $database = DatabaseFactory::getInstance("pdo");
 $twig = TemplateFactory::getInstance("twig");
 
-if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
-    echo $twig->render('index.html', ['user_id' => $_SESSION["user_id"], 'go' => 'here']);
-} else {
-    header("location: login.php");
-}
+echo $twig->render('index.html', ['user_id' => $_SESSION["user_id"], 'go' => 'here']);
 
 // printf("Now: %s", Carbon::now());
